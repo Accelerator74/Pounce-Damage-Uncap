bc3176c0539fcf02bf29407b23edeea980a1c55c
Accelerator74 <accelerator74@gmail.com>
2020-09-29 13:19:50 +0500

    Update pounce_damage_uncap.cpp

--- pounce_damage_uncap.cpp
+++ pounce_damage_uncap.cpp
@@ -88,8 +88,8 @@ const char c_sPattern[] = "\xF3\x0F\x10\x1D\x2A\x2A\x2A\x2A\xF3\x0F\x59\xC0\xF3\
 // comiss  xmm0, ds:flt_BAA9BC (masked)
 // jbe     loc_95EF20
 // movss   xmm1, [ebp+var_19C] (only the instruction)
-// 0F 2F 05 ? ? ? ? 0F 86 90 02 00 00 F3 0F 10
-const char c_sPattern[] = "\x0F\x2F\x05\x2A\x2A\x2A\x2A\x0F\x86\x90\x02\x00\x00\xF3\x0F\x10";
+// 0F 2F 05 ? ? ? ? 0F 86 ED 04 00 00 F3 0F 10
+const char c_sPattern[] = "\x0F\x2F\x05\x2A\x2A\x2A\x2A\x0F\x86\xED\x04\x00\x00\xF3\x0F\x10";
 
 //comiss  xmm0, ds:fl_300
 // instruction starts at sig start, addr is 3 bytes into instruction
