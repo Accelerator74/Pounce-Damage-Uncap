6f957be450ae383e96d9436d60fe09aa46ca4cb0
Accelerator74 <accelerator74@gmail.com>
2021-06-17 16:47:51 +0500

    Update pounce_damage_uncap.cpp
    Thanks A1mDev

--- pounce_damage_uncap.cpp
+++ pounce_damage_uncap.cpp
@@ -88,27 +88,27 @@ const char c_sPattern[] = "\xF3\x0F\x10\x1D\x2A\x2A\x2A\x2A\xF3\x0F\x59\xC0\xF3\
 // comiss  xmm0, ds:flt_BAA9BC (masked)
 // jbe     loc_95EF20
 // movss   xmm1, [ebp+var_19C] (only the instruction)
-// 0F 2F 05 ? ? ? ? 0F 86 B4 02 00 00 A1 5C 98
-const char c_sPattern[] = "\x0F\x2F\x05\x2A\x2A\x2A\x2A\x0F\x86\xB4\x02\x00\x00\xA1\x5C\x98";
+// 0F 2F 05 ? ? ? ? 0F 86 B4 02 00 00 A1 5C
+const char c_sPattern[] = "\x0F\x2F\x05\x2A\x2A\x2A\x2A\x0F\x86\xB4\x02\x00\x00\xA1\x5C";
 
 //comiss  xmm0, ds:fl_300
 // instruction starts at sig start, addr is 3 bytes into instruction
 #define _MinRangeAddrOffset 3
 
 //comiss  xmm1, ds:fl_1000
-// 26 bytes from sig is this instruction
-// address operand is 3 bytes into that instruction = 29 bytes
-#define _MaxRangeAddrOffset 29
+// 18 bytes from sig is this instruction
+// address operand is 3 bytes into that instruction = 21 bytes
+#define _MaxRangeAddrOffset 21
 
 //mulss   xmm0, ds:fl_1_div_700
-// 40 bytes from sig is this instruction
-// address operand is 4 bytes into that instruction = 44 bytes
-#define _RangeScaleFactorAddrOffset 44
+// 32 bytes from sig is this instruction
+// address operand is 4 bytes into that instruction = 36 bytes
+#define _RangeScaleFactorAddrOffset 36
 
 //addss   xmm0, ds:fl_neg_300
-// 64 bytes from sig is this instruction
-// address operand is 4 bytes into that instruction = 68 bytes
-#define _NegativeMinRangeAddrOffset 68
+// 60 bytes from sig is this instruction
+// address operand is 3 bytes into that instruction = 63 bytes
+#define _NegativeMinRangeAddrOffset 63
 
 #endif
 
